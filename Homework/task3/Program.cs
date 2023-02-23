2db97dab7a95d817f5be0935370a14ee5ee74b12
Artem Nogin <artemka-174.ru@mail.ru>
2023-02-23 16:17:59 +0300

    task3

---
+++ Homework/task3/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+int Usernum(string msg)
+{
+    System.Console.Write($"{msg} >");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+bool Weekd(int numday)
+{
+    if (numday >5)
+    {
+        return true;
+    }
+
+    return false;
+}
+
+bool Validate(int day)
+  {
+
+    if (day >0 && day <=7)
+{
+     return true;
+}
+    System.Console.WriteLine("Попробуй еще раз");
+    return false;
+  } 
+
+int weekday=Usernum("Введите номер дня недели ");
+if (Validate(weekday))
+ {
+    if (Weekd(weekday))
+    {
+        System.Console.WriteLine($" {weekday} Выхолной!");
+    }
+    else
+    {
+        System.Console.WriteLine($" {weekday} Будни!");
+    }
+ }
